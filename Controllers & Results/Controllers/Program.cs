e8b9205914b46c2e6db2e95a311f096f4d1de050
raj9680 <aryanr222@gmail.com>
2023-12-13 19:47:05 +0530

    message

---
+++ Controllers & Results/Controllers/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers(); // adds all the classes as services
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers(); 
+});
+
+
+app.Run();
+
+
---
+++ Controllers & Results/StatusCodeResult/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatusCodeResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
