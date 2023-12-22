11b552e140c74152bdc4b432f7e77074f7b63fd3
raj9680 <aryanr222@gmail.com>
2023-12-22 17:16:16 +0530

    htp

---
+++ Model Binding & Validations/ModelClass/Models/BookModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelClass.Models
+{
+    public class BookModel
+    {
+        public int? BookId { get; set; }
+        public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book id: {BookId}, Author: {Author}";
+        }
+    }
+}
